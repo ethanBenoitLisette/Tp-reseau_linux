720cf05fabe9ddf3d0489bb3720071d9a986bc11
bethan <ethan.benoitlisette@ynov.com>
2023-11-06 11:06:25 +0100

    ecoute et reponse

---
+++ TP4/bs_client_I1.py
@@ -0,0 +1,23 @@
+import socket
+
+# On définit la destination de la connexion
+host = '<10.0.3.17>'  # IP du serveur
+port = 13337               # Port choisir par le serveur
+
+# Création de l'objet socket de type TCP (SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connexion au serveur
+s.connect((host, port))
+
+# Envoi de data bidon
+s.sendall(b'SALUT MEC')
+
+# On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
+data = s.recv(1024)
+
+# On libère le socket TCP
+s.close()
+
+# Affichage de la réponse reçue du serveur
+print(f"Le serveur a répondu {repr(data)}")
--- TP4/bs_server_I1.py
+++ TP4/bs_server_I1.py
@@ -2,7 +2,7 @@ import socket
 
 # On choisit une IP et un port où on va écouter
 host = '' # string vide signifie, dans ce conetxte, toutes les IPs de la machine
-port = 8888 # port choisi arbitrairement
+port = 13337 # port choisi arbitrairement
 
 # On crée un objet socket
 # SOCK_STREAM c'est pour créer un socket TCP (pas UDP donc)
