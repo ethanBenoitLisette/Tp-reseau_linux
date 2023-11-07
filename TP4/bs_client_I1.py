7d0bf8270391d165131620d34ae2bc1e66b34994
bethan <ethan.benoitlisette@ynov.com>
2023-11-07 08:40:39 +0100

    ecoute et reponse

--- TP4/bs_client_I1.py
+++ TP4/bs_client_I1.py
@@ -11,7 +11,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
 # Envoi de data bidon
-s.sendall(b'SALUT MEC')
+s.sendall(b'Meooooo !')
 
 # On reçoit 1024 bytes qui contiennent peut-être une réponse du serveur
 data = s.recv(1024)
