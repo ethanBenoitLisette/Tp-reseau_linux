ed0514aaf6a8095f8d15132ef612560276abce67
ethanBenoitLisette <112951754+ethanBenoitLisette@users.noreply.github.com>
2023-12-07 21:57:04 +0100

    ui

--- TP6/partie-2/chat_client_ii_2.py
+++ TP6/partie-2/chat_client_ii_2.py
@@ -1,35 +1,19 @@
 import socket
-import asyncio
 
-async def receive_messages(client_socket):
-    try:
-        while True:
-            # Attendre la réception d'un message du serveur
-            data = await loop.sock_recv(client_socket, 1024)
-            print(f"Message du serveur : {data.decode()}")
-    except (socket.error, KeyboardInterrupt):
-        pass
-
-async def send_message(clients, message):
-    for client in clients:
-        try:
-            client.write(message.encode())
-            await client.drain()
-        except:
-            pass
-
-async def main():
-    host = '10.0.3.17'
-    port = 8888
-
-    loop = asyncio.get_event_loop()  # Initialiser la boucle d'événements ici
+def main():
+    host = '10.0.3.17'  # L'adresse IP du serveur
+    port = 8888         # Le port utilisé par le serveur
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((host, port))
 
-        # Lancer une tâche asynchrone pour recevoir des messages du serveur
-        await receive_messages(client_socket)
+        # Envoyer "Hello" au serveur
+        client_socket.sendall(b'Hello')
+
+        # Attendre la réponse du serveur
+        data = client_socket.recv(1024)
+
+    print(f"Réponse du serveur : {data.decode()}")
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    main()
--- TP6/partie-2/chat_server_ii_2.py
+++ TP6/partie-2/chat_server_ii_2.py
@@ -1,20 +1,23 @@
 import asyncio
 
-async def handle_client(reader, writer, clients):
+async def handle_client(reader, writer):
+    # Récupérer les informations sur le client
     client_address = writer.get_extra_info('peername')
     print(f"Client connecté : {client_address}")
-    clients.add(writer)
 
+    # Envoyer un message de bienvenue
     welcome_message = f"Hello {client_address[0]}:{client_address[1]}\n"
     writer.write(welcome_message.encode())
     await writer.drain()
 
     try:
         while True:
+            # Lire le message du client
             data = await reader.read(100)
             if not data:
                 break
 
+            # Afficher le message du client
             message = data.decode()
             print(f"Message du client {client_address}: {message}")
 
@@ -25,41 +28,20 @@ async def handle_client(reader, writer, clients):
     except asyncio.CancelledError:
         pass
     finally:
+        # Fermer la connexion avec le client
         print(f"Client déconnecté : {client_address}")
-        clients.remove(writer)
         writer.close()
-
-async def send_message(clients, message):
-    for client in clients:
-        try:
-            client.write(message.encode())
-            await client.drain()
-        except:
-            pass
 
 async def main():
     server = await asyncio.start_server(
         handle_client, '', 8888)
 
+    # Afficher l'adresse du serveur
     addr = server.sockets[0].getsockname()
     print(f'Serveur en écoute sur {addr}')
 
-    clients = set()
-
-    try:
-        while True:
-            # Attendre un message du terminal (console)
-            server_message = input("Saisissez un message pour les clients : ")
-
-            # Envoyer le message à tous les clients
-            await send_message(clients, server_message)
-
-    except KeyboardInterrupt:
-        pass
-    finally:
-        for client in clients:
-            client.close()
+    async with server:
+        await server.serve_forever()
 
 if __name__ == "__main__":
     asyncio.run(main())
-
