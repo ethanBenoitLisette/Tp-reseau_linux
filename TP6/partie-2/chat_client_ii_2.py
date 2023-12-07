843fa4d573c7783552ac25baa5b13894a638fd40
ethanBenoitLisette <112951754+ethanBenoitLisette@users.noreply.github.com>
2023-12-07 21:43:58 +0100

    ui

--- TP6/partie-2/chat_client_ii_2.py
+++ TP6/partie-2/chat_client_ii_2.py
@@ -1,4 +1,14 @@
 import socket
+import asyncio
+
+async def receive_messages(client_socket):
+    try:
+        while True:
+            # Attendre la réception d'un message du serveur
+            data = client_socket.recv(1024)
+            print(f"Message du serveur : {data.decode()}")
+    except (socket.error, KeyboardInterrupt):
+        pass
 
 def main():
     host = '10.0.3.17'
@@ -7,16 +17,18 @@ def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((host, port))
 
-        while True:
-            # Envoyer un message au serveur
-            message = input("Saisissez un message (ou 'exit' pour quitter) : ")
-            if message.lower() == 'exit':
-                break
-            client_socket.sendall(message.encode())
+        # Lancer une tâche asynchrone pour recevoir des messages du serveur
+        asyncio.create_task(receive_messages(client_socket))
 
-            # Attendre la réponse du serveur
-            data = client_socket.recv(1024)
-            print(f"Réponse du serveur : {data.decode()}")
+        try:
+            while True:
+                # Envoyer un message au serveur
+                message = input("Saisissez un message (ou 'exit' pour quitter) : ")
+                if message.lower() == 'exit':
+                    break
+                client_socket.sendall(message.encode())
+        except KeyboardInterrupt:
+            pass
 
 if __name__ == "__main__":
     main()
