61875ee9c94f88a0eee13438c350c63c83ea7898
ethanBenoitLisette <112951754+ethanBenoitLisette@users.noreply.github.com>
2023-12-07 21:42:07 +0100

    ui

--- TP6/partie-2/chat_server_ii_2.py
+++ TP6/partie-2/chat_server_ii_2.py
@@ -1,8 +1,9 @@
 import asyncio
 
-async def handle_client(reader, writer):
+async def handle_client(reader, writer, clients):
     client_address = writer.get_extra_info('peername')
     print(f"Client connecté : {client_address}")
+    clients.add(writer)
 
     welcome_message = f"Hello {client_address[0]}:{client_address[1]}\n"
     writer.write(welcome_message.encode())
@@ -25,7 +26,16 @@ async def handle_client(reader, writer):
         pass
     finally:
         print(f"Client déconnecté : {client_address}")
+        clients.remove(writer)
         writer.close()
+
+async def send_message(clients, message):
+    for client in clients:
+        try:
+            client.write(message.encode())
+            await client.drain()
+        except:
+            pass
 
 async def main():
     server = await asyncio.start_server(
@@ -34,8 +44,22 @@ async def main():
     addr = server.sockets[0].getsockname()
     print(f'Serveur en écoute sur {addr}')
 
-    async with server:
-        await server.serve_forever()
+    clients = set()
+
+    try:
+        while True:
+            # Attendre un message du terminal (console)
+            server_message = input("Saisissez un message pour les clients : ")
+
+            # Envoyer le message à tous les clients
+            await send_message(clients, server_message)
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for client in clients:
+            client.close()
 
 if __name__ == "__main__":
     asyncio.run(main())
+
