17b6e726b4f75932d92aeff44295bda29091b63e
ethanBenoitLisette <112951754+ethanBenoitLisette@users.noreply.github.com>
2023-12-14 11:46:57 +0100

    ui

--- TP7/ws_i_1_client.py
+++ TP7/ws_i_1_client.py
@@ -2,20 +2,14 @@ import asyncio
 import websockets
 
 async def user_interaction():
-    uri = "ws://localhost:8765"
+    uri = "ws://10.0.3.17:8888"
     async with websockets.connect(uri) as websocket:
         try:
             while True:
-                # Saisie utilisateur
                 user_input = input("Saisissez une chaîne à envoyer au serveur (ou 'exit' pour quitter) : ")
-
                 if user_input.lower() == 'exit':
                     break
-
-                # Envoyer le message au serveur
                 await websocket.send(user_input)
-
-                # Recevoir la réponse du serveur
                 response = await websocket.recv()
                 print(f"Client a reçu : {response}")
 
@@ -24,4 +18,3 @@ async def user_interaction():
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(user_interaction())
-
--- TP7/ws_i_1_server.py
+++ TP7/ws_i_1_server.py
@@ -3,15 +3,9 @@ import websockets
 
 async def handle_client(websocket, path):
     try:
-        
         while True:
-            # Recevoir le message du client
             message = await websocket.recv()
-
-            # Afficher le message côté serveur
             print(f"Serveur a reçu : {message}")
-
-            # Répondre au client
             response = f"Hello client ! Received \"{message}\""
             await websocket.send(response)
 
@@ -19,7 +13,6 @@ async def handle_client(websocket, path):
         print("Client déconnecté.")
 
 if __name__ == "__main__":
-    start_server = websockets.serve(handle_client, "localhost", 8765)
-
+    start_server = websockets.serve(handle_client, "10.0.3.17", 8888)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
